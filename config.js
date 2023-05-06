b33999f9b2c8b48efdbde836b3d841f50be0b802
PandarusAnon <pandarusanon@proton.me>
2023-05-06 17:24:27 +0200

    Add clarification to config comments

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ const config = {
     CHANNEL: "C1234D4567S",
     CLAUDE_USER: "U1234A568BC",
 
-    // The final message we send with an @Claude ping to trigger his response.
+    // The final message we send with an @Claude ping to trigger his response. You don't need to add the @Claude bit, Slaude does that automatically.
     // Can be whatever you want it to be but keep in mind that it might result in unpredictable responses.
-    // Anything we put here will eat into our max available tokens so keep it brief. Anything above ~12000 characters will get cut off by Slack
+    // Anything we put here will eat into our max available tokens so keep it brief. Anything above ~12000 characters will get cut off by Slack.
     PING_MESSAGE: "Assistant:",
 
     PORT: 5004
